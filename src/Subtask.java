7e8d80cd6b57ffcd98409e61132696f0ca8aef59
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2025-12-21 01:37:24 +0300

    feat: создать класс Subtask

---
+++ src/Subtask.java
@@ -0,0 +1,7 @@
+public class Subtask extends Task {
+    protected int epicId;
+
+    public Subtask(String name, String description, int id, String status) {
+        super(name, description, id, status);
+    }
+}
