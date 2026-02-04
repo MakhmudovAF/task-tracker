ad6afe60b92944bc680af34cbb07100ffe752ee8
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2026-02-05 00:25:58 +0300

    feat: реализовать возможность хранить задачи всех типов

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -1,7 +1,18 @@
 package manager;
 
+import model.Epic;
+import model.Subtask;
+import model.Task;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class TaskManager {
     private int nextId = 1;
+
+    private final Map<Integer, Task> tasks = new HashMap<>();
+    private final Map<Integer, Epic> epics = new HashMap<>();
+    private final Map<Integer, Subtask> subtasks = new HashMap<>();
 
     private int generateId() {
         return nextId++;
