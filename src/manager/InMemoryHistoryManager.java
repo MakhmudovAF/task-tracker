3375f07fbedd5d08c39a944757170058479ff8b3
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2026-02-05 01:12:22 +0300

    feat: реализовать реализацию интерфейса истории просмотров задач

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -1,4 +1,28 @@
 package manager;
 
-public class InMemoryHistoryManager {
+import model.Task;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    private static final int LIMIT = 10;
+    private final LinkedList<Task> history = new LinkedList<>();
+
+    @Override
+    public void add(Task task) {
+        if (task == null) {
+            return;
+        }
+        history.addLast(task);
+        if (history.size() > LIMIT) {
+            history.removeFirst();
+        }
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return new ArrayList<>(history);
+    }
 }
