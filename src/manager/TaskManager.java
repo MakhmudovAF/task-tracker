7f55614488d6eb032fac574d83ecca7ce1c916e4
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2026-02-05 00:26:58 +0300

    feat: реализовать методы для работы с типом задачи Task

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -1,10 +1,13 @@
 package manager;
 
+import enums.Status;
 import model.Epic;
 import model.Subtask;
 import model.Task;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TaskManager {
@@ -16,5 +19,37 @@ public class TaskManager {
 
     private int generateId() {
         return nextId++;
+    }
+
+    // ---------------- TASK ----------------
+    public List<Task> getAllTasks() {
+        return new ArrayList<>(tasks.values());
+    }
+
+    public void deleteAllTasks() {
+        tasks.clear();
+    }
+
+    public Task getTaskById(int id) {
+        return tasks.get(id);
+    }
+
+    public Task createTask(Task task) {
+        int id = generateId();
+        task.setId(id);
+        tasks.put(id, task);
+        return task;
+    }
+
+    public Task updateTask(Task task) {
+        if (!tasks.containsKey(task.getId())) {
+            return null;
+        }
+        tasks.put(task.getId(), task);
+        return task;
+    }
+
+    public Task deleteTaskById(int id) {
+        return tasks.remove(id);
     }
 }
