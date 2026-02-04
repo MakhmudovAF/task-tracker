b6d59848c6e278a7fc85d284f919d907f473f782
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2026-02-05 00:20:53 +0300

    feat: реализовать тип подзадачу для класса Epic - класс Subtask

---
+++ src/model/Subtask.java
@@ -0,0 +1,32 @@
+package model;
+
+import enums.Status;
+
+public class Subtask extends Task {
+    private final int epicId;
+
+    public Subtask(String name, String description, Status status, int epicId) {
+        super(name, description, status);
+        this.epicId = epicId;
+    }
+
+    public Subtask(int id, String name, String description, Status status, int epicId) {
+        super(id, name, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "id=" + id +
+                ", epicId=" + epicId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
