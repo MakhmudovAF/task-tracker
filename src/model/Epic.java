e099518aac044f02f9369376c10f2b47a9e9a8c0
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2026-02-05 00:19:30 +0300

    feat: реализовать масштабный тип задачи - класс Epic

---
+++ src/model/Epic.java
@@ -0,0 +1,45 @@
+package model;
+
+import enums.Status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Epic extends Task {
+    private final List<Integer> subtaskIds = new ArrayList<>();
+
+    public Epic(String name, String description) {
+        super(name, description, Status.NEW);
+    }
+
+    public Epic(int id, String name, String description) {
+        super(id, name, description, Status.NEW);
+    }
+
+    public List<Integer> getSubtaskIds() {
+        return new ArrayList<>(subtaskIds);
+    }
+
+    public void addSubtaskId(int subtaskId) {
+        subtaskIds.add(subtaskId);
+    }
+
+    public void removeSubtaskId(int subtaskId) {
+        subtaskIds.remove((Integer) subtaskId);
+    }
+
+    public void clearSubtaskIds() {
+        subtaskIds.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "Epic{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", subtaskIds=" + subtaskIds +
+                '}';
+    }
+}
